f8edd054906d932b9ac0cfa3fe0e332eec9aad5f
REDDII <dhnotes@gmail.com>
2026-02-16 17:29:13 +0100

    Time: 549 ms (15.79%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,6 +1,3 @@
--- Write your PostgreSQL query statement below
 SELECT e1.name as Employee
-FROM employee as e1 , employee as e2 
-where (e1.managerid = e2.id and e1.salary > e2.salary );
-
-    
+FROM employee e1 , employee e2 
+where (e1.managerid = e2.id and e1.salary > e2.salary ) 
