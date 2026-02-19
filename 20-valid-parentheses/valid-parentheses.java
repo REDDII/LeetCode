64e5c7c7dd83b0fec6bc8ead4ecb08378d469e98
REDDII <dhnotes@gmail.com>
2026-02-19 15:55:57 +0100

    Time: 1 ms (99.69%) | Memory: 43.3 MB (58.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -5,21 +5,21 @@ class Solution {
         int pile = -1;
 
         if (nbre == 1 || nbre % 2 != 0 ) return false;
-        System.out.println("init l = " + nbre);
-        System.out.println("init pile = " + pile);
+        // System.out.println("init l = " + nbre);
+        // System.out.println("init pile = " + pile);
         for (int i=0; i<nbre; i++) {
             char c = s.charAt(i);
-            System.out.println("c = " + c);
+            // System.out.println("c = " + c);
             if (c == '(' || c == '{' || c == '[') {
                 tab[++pile] = c;
-                System.out.println("pile = " + pile);
+                // System.out.println("pile = " + pile);
             }
 
             else {
-                System.out.println("pile = " + pile);
+                // System.out.println("pile = " + pile);
                 if (pile == -1) return false;
                 char open = tab[pile--];
-                System.out.println("précédent à supp = " + open);
+                // System.out.println("précédent à supp = " + open);
 
                 if ((c == ')' && open != '(') ||
                     (c == '}' && open != '{') ||
@@ -28,7 +28,7 @@ class Solution {
                 }
             }
         }
-        System.out.println(pile);
+        // System.out.println(pile);
         return pile == -1;
     }
 }
